4b36827fb585a2c8e9c4f98bdfca6c23106e9444
Uyimolo <108616062+Uyimolo@users.noreply.github.com>
2022-10-09 15:28:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const showHide = (...args) => {
     args.forEach((arg) => arg.classList.toggle("active"));
   };
-
-  document.querySelector(".menu").addEventListener("click", () => {
-    const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar");
     const open = document.querySelector(".open")
     const container = document.querySelector("body");
+
+  document.querySelector(".menu").addEventListener("click", () => {
     showHide(navbar,open,container);
   })
   document.querySelectorAll(".nav-link").forEach((navLink) => {
