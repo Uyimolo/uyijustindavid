088d8a2164cc8f50011e653a3198b1b7e7d47d2d
Uyimolo <108616062+Uyimolo@users.noreply.github.com>
2022-10-23 09:01:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ document.querySelectorAll(".nav-link").forEach((navLink) => {
     showHide(navbar, menu, curtain);
   });
 });
+
+//scroll animation
+function reveal(e) {
+  var reveals = document.querySelectorAll(".reveal");
+  
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+//run page animation function on page load
+reveal();
