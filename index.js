08240c9ae57fb99a41cc484c9ce59a61aaf94a88
uyimolo <uyijustindavid@gmail.com>
2023-01-02 07:52:32 +0100

    latest style changes, changed landing page alignment, added photo to landing page, made a very wierd project card lol

--- index.js
+++ index.js
@@ -19,21 +19,32 @@ document.querySelectorAll(".nav-link").forEach((navLink) => {
     showHide(navbar, menu, curtain);
   });
 });
+curtain.addEventListener("click", () => {
+  showHide(navbar, container, menu, curtain);
+});
 
-document.querySelectorAll(".show-work").forEach(work => work.addEventListener("click", (e)=> {
-  const clicked = e.target
-if(clicked.classList.contains("project-image")){
-  clicked.nextElementSibling.classList.add("active")
-  clicked.classList.add("active")
-  console.log(e.target)
-}
-else{
-  clicked.classList.remove("active")
-  console.log(clicked)
-  clicked.previousElementSibling.classList.remove("active")
-}
+document.querySelectorAll(".show-work").forEach((work) =>
+  work.addEventListener("click", (e) => {
+    const clicked = e.target;
+    const alreadyClicked = [...document.querySelectorAll(".clicked")];
+    // console.log(alreadyClicked
+    alreadyClicked.forEach((item) => {
+      if (e.target !== item) {
+        item.classList.remove("clicked");
+      }
+    });
 
-}))
+    if (clicked.classList.contains("project-image")) {
+      clicked.nextElementSibling.classList.add("clicked");
+      clicked.classList.add("clicked");
+      // console.log(e.target)
+    } else {
+      clicked.classList.remove("clicked");
+      // console.log(clicked)
+      clicked.previousElementSibling.classList.remove("clicked");
+    }
+  })
+);
 
 const reveal = (e) => {
   const reveals = document.querySelectorAll(".reveal");
